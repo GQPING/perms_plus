d4b787f401a9d857d8949f3c62f0db84d898bb42
chengxianwei <1878776239@qq.com>
2022-02-09 10:56:25 +0800

    更新

--- src/main/java/com/perms/demo/utils/SecurityUtils.java
+++ src/main/java/com/perms/demo/utils/SecurityUtils.java
@@ -1,9 +1,7 @@
 package com.perms.demo.utils;
 
-import com.perms.demo.app.domain.AjaxResult;
 import com.perms.demo.app.domain.LoginUser;
 import com.perms.demo.exception.BaseException;
-import com.perms.demo.exception.ServiceException;
 import com.perms.demo.utils.constant.HttpStatus;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -11,61 +9,48 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 /**
  * 安全服务工具类
+ *
  * @author cxw
  * @date 2022/2/8
  */
-public class SecurityUtils
-{
+public class SecurityUtils {
     /**
      * 用户ID
      **/
-    public static Long getUserId()
-    {
-        try
-        {
+    public static Long getUserId() {
+        try {
             return getLoginUser().getUserId();
-        }
-        catch (Exception e)
-        {
-            throw new BaseException(HttpStatus.UNAUTHORIZED,"获取用户ID异常");
+        } catch (Exception e) {
+            throw new BaseException(HttpStatus.UNAUTHORIZED, "获取用户ID异常");
         }
     }
 
     /**
      * 获取用户账户
      **/
-    public static String getUsername()
-    {
-        try
-        {
+    public static String getUsername() {
+        try {
             return getLoginUser().getUsername();
-        }
-        catch (Exception e)
-        {
-            throw new BaseException( HttpStatus.UNAUTHORIZED,"获取用户账户异常");
+        } catch (Exception e) {
+            throw new BaseException(HttpStatus.UNAUTHORIZED, "获取用户账户异常");
         }
     }
 
     /**
      * 获取用户
      **/
-    public static LoginUser getLoginUser()
-    {
-        try
-        {
+    public static LoginUser getLoginUser() {
+        try {
             return (LoginUser) getAuthentication().getPrincipal();
-        }
-        catch (Exception e)
-        {
-            throw new BaseException(HttpStatus.UNAUTHORIZED,"获取用户信息异常");
+        } catch (Exception e) {
+            throw new BaseException(HttpStatus.UNAUTHORIZED, "获取用户信息异常");
         }
     }
 
     /**
      * 获取Authentication
      */
-    public static Authentication getAuthentication()
-    {
+    public static Authentication getAuthentication() {
         return SecurityContextHolder.getContext().getAuthentication();
     }
 
@@ -75,8 +60,7 @@ public class SecurityUtils
      * @param password 密码
      * @return 加密字符串
      */
-    public static String encryptPassword(String password)
-    {
+    public static String encryptPassword(String password) {
         BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
         return passwordEncoder.encode(password);
     }
@@ -84,12 +68,11 @@ public class SecurityUtils
     /**
      * 判断密码是否相同
      *
-     * @param rawPassword 真实密码
+     * @param rawPassword     真实密码
      * @param encodedPassword 加密后字符
      * @return 结果
      */
-    public static boolean matchesPassword(String rawPassword, String encodedPassword)
-    {
+    public static boolean matchesPassword(String rawPassword, String encodedPassword) {
         BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
         return passwordEncoder.matches(rawPassword, encodedPassword);
     }
@@ -100,8 +83,7 @@ public class SecurityUtils
      * @param userId 用户ID
      * @return 结果
      */
-    public static boolean isAdmin(Long userId)
-    {
+    public static boolean isAdmin(Long userId) {
         return userId != null && 1L == userId;
     }
 }
